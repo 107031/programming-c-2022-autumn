fbc0ae0964aa4bc3bdf30ebca7e5a1c7ce8f6532
107031 <nishanov90@gmail.com>
2022-10-02 02:35:02 +0300

    task 10 done

--- 2022.09.15-Homework-1/Task 10/Source.cpp
+++ 2022.09.15-Homework-1/Task 10/Source.cpp
@@ -1,11 +1,10 @@
-//I do not know how to do it
-
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	int a = 0;
-	int b = 0;
-	std::cin >> a >> b;
-	std::cout << a / b;
-	return EXIT_SUCCESS;
+    int x = 0;
+    int y = 0;
+    std::cin >> x >> y;
+    int mod = ((x - y) % 2 + (x - y + 1) % 2) % 2 * (x - y);
+    std::cout << (x + y + mod) / 2;
+    return EXIT_SUCCESS;
 }
