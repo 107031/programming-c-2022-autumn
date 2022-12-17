82930d9cff267d6fd04a7dc51ab39ff8c201f862
107031 <nishanov90@gmail.com>
2022-12-17 22:16:53 +0300

    done task10

--- 2022.12.05-Homework-8/Task 10/Source.cpp
+++ 2022.12.05-Homework-8/Task 10/Source.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
-#include <string>
 
-void DeletingBranch(int n) {
+int array[1000000]{ 0 };
 
+int DeletingBranch(int n) {
+	if (array[n] == -1) {
+		return 1;
+	}
+	else {
+		return DeletingBranch(array[n]) + 1;
+	}
 }
 
 int main(int argc, char* argv[]) {
 	int n = 0;
+	int k = 0;
+	int l = 0;
+	int d = 0;
+	for (int i = 0; i < 1000000; i++) {
+		array[i] = -1;
+ 	}
 	std::cin >> n;
-	DeletingBranch(n);
+	for (int i = 0; i < n; i++) {
+		std::cin >> k >> l;
+		array[l] = k;
+	}
+	std::cin >> n;
+	std::cout << DeletingBranch(n);
 	return EXIT_SUCCESS;
 }
